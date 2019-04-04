b7f4b01f7502f5a3500d8c9401ba26745fad360f
dit02 <dit@imac-7.local>
2019-04-04 16:25:18 +0900

    Initial Commit

---
+++ UITextField 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextField 02
+//
+//  Created by dit02 on 2019. 4. 4..
+//  Copyright © 2019년 dit02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
